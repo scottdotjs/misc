ac061ed881f2c4f052359ebd6954c284a9a1544d
Scott Martin <scott@scottmartin.systems>
2022-10-13 22:50:27 +0100

    [ia-78s] Get download links for both sides

--- ia-78s/other-sides.pl
+++ ia-78s/other-sides.pl
@@ -3,7 +3,16 @@
 use warnings;
 use strict;
 
+use utf8;
+use open qw(:std :utf8);
+
+use feature 'signatures';
+no warnings 'experimental::signatures';
+
+use HTML5::DOM;
 use WWW::Mechanize;
+
+use constant TRUE => 1;
 
 my $mech = WWW::Mechanize->new();
 $mech->agent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1');
@@ -15,25 +24,47 @@ $| = 1;
 # Output number of items to process
 # Do a progress bar thing (find a fancy module for it)
 
-while (<DATA>) {
-	$mech->get($_);
+my $parser = HTML5::DOM->new;
+
+sub get_html ($url) {
+	$mech->get($url);
 
 	if ($mech->success) {
-		my $other_side = '';
-	 	my ($other_side_found) = $mech->response->decoded_content =~ m{The recording on the other side of this disc: <a href="(.*?)" rel};
-
-	 	if ($other_side_found) {
-	 		$other_side = 'https://archive.org' . $other_side_found;
-	 	}
-
-	 	chomp(my $url = $_);
-	 	print $OUT "$url\t$other_side\n";
+		return $mech->response->decoded_content;
 	} else {
-		print STDERR $mech->response->status_line, "\n";
+		die $mech->response->status_line;
+	}
+}
+
+sub fetch ($url, $get_other_side = 0) {
+	print "Fetch: $url\n";
+
+	my $html = get_html($url);
+	my $tree = $parser->parse($html);
+
+	my $download = 'https://archive.org' . $tree->querySelector('#quickdown1 > .format-file a')->attr('href');
+
+	print "$download\n\n";
+	print $OUT "$url\t$download\n";
+
+	if ($get_other_side) {
+		my $other_side_found = $tree->querySelector('p.content')->nextSibling->querySelector('a')->attr('href');
+
+		if ($other_side_found) {
+			fetch('https://archive.org' . $other_side_found);
+		}
 	}
 
- 	sleep 2;
+}
+
+while (<DATA>) {
+	chomp;
+
+	fetch($_, TRUE);
+
+	sleep 2;
 }
 
 __DATA__
 https://archive.org/details/78_stumblin_the-s-m-o-o-t-h-music-of-larry-fotine-cathy-cordovan-zez-confrey_gbia0409848a
+https://archive.org/details/78_aint-it-a-crime_julia-lee-and-her-boy-friends-julia-lee-baby-lovett-vic-dickenson_gbia0206089b
