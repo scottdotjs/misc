507d0364ed2c26e711d41091057a446b0648c258
Scott Martin <scott@scottmartin.systems>
2022-10-16 00:04:07 +0100

    [ia-78s] Display progress bar in terminal, separate track artist and title in output

--- ia-78s/get-links.pl
+++ ia-78s/get-links.pl
@@ -10,10 +10,10 @@ use feature 'signatures';
 no warnings 'experimental::signatures';
 
 use HTML5::DOM;
+use Progress::Any '$progress';
+use Progress::Any::Output 'TermProgressBarColor';
 use URI::Escape;
 use WWW::Mechanize;
-
-use constant TRUE => 1;
 
 my $mech = WWW::Mechanize->new();
 $mech->agent('Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) CriOS/69.0.3497.105 Mobile/15E148 Safari/605.1');
@@ -27,44 +27,51 @@ $| = 1;
 
 my $parser = HTML5::DOM->new;
 
+my $target;
+
 sub get_html ($url) {
 	$mech->get($url);
 
-	if ($mech->success) {
-		return $mech->response->decoded_content;
-	} else {
+	if (!$mech->success) {
 		die $mech->response->status_line;
 	}
+
+	return $mech->response->decoded_content;
 }
 
-sub fetch_side_a($url) {
-	fetch($url, 'A', TRUE);
-}
+sub fetch (%options) {
+	my $url   = $options{'url'  };
+	my $side  = $options{'side' };
+	my $disc  = $options{'disc' };
+	my $total = $options{'total'};
 
-sub fetch_side_b($url) {
-	fetch($url, 'B')
-}
-
-sub fetch ($url, $side, $get_other_side = 0) {
 	die "Not an IA URL: $url" if $url !~ m{https://archive.org/};
+
+	$progress->update(message => "Processing disc $disc/$total side $side ");
 
 	my $html = get_html($url);
 	my $tree = $parser->parse($html);
 
 	my $download = 'https://archive.org' . $tree->querySelector('#quickdown1 > .format-file a')->attr('href');
-	my ($track_title) = uri_unescape($download) =~ m{.*/(.*?)\.flac$};
+	my ($track_info) = uri_unescape($download) =~ m{.*/(.*?)\.flac$};
+	my ($title, $artist) = split(/ - /, $track_info);
 
-	print "$side. $track_title\n";
-	print $OUT "$url\t$download\n";
+	print $OUT "$url\t$artist\t$title\t$download\n";
 
-	if ($get_other_side) {
+	if ($side eq 'A') {
 		my $other_side_found = $tree->querySelector('p.content')->nextSibling->querySelector('a')->attr('href');
 
 		if ($other_side_found) {
-			fetch_side_b('https://archive.org' . $other_side_found);
+			$progress->target(++$target);
+
+			fetch((
+				url   => 'https://archive.org' . $other_side_found,
+				side  => 'B',
+				disc  => $disc,
+				total => $total
+			));
 		}
 	}
-
 }
 
 sub deshorten ($url) {
@@ -79,25 +86,33 @@ sub deshorten ($url) {
 	}
 }
 
-chomp(my @input = <DATA>);
+sub main {
+	chomp(my @input = <DATA>);
 
-print $#input + 1 . " discs to process.\n";
+	my $total = $#input + 1;
+	$target = $total;
 
-my $count = 0;
+	$progress->target($target);
 
-foreach my $item (@input) {
-	$count++;
+	my $disc = 0;
 
-	print "\n[$count]\n";
+	print $OUT "Track page URL\tArtist\tTitle\tDownload URL\n";
 
-	my $url = $item;
+	foreach my $item (@input) {
+		fetch((
+			url   => ($item =~ /t\.co/) ? deshorten($item) : $item,
+			side  => 'A',
+			disc  => ++$disc,
+			total => $total
+		));
 
-	$url = deshorten($url) if ($url =~ /t\.co/);
+		sleep 2;
+	}
 
-	fetch_side_a($url);
-
-	sleep 2;
+	$progress->finish(message => 'Done.');
 }
+
+main();
 
 # Example tweet format:
 # the music goes round & round (1956) - paul gayton https://t.co/cFzMh3sVAt https://t.co/ignorethislink
